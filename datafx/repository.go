f506360c217607c505369414f235fbbba74db5fc
Eser Ozvataf <eser@acikyazilim.com>
2025-02-28 01:20:13 +0300

    feat: add generic Repository type with query management methods

--- datafx/repository.go
+++ datafx/repository.go
@@ -1,17 +1,17 @@
 package datafx
 
 type Repository[T any] struct {
-	Queries *T
+	Queries T
 }
 
-func NewRepository[T any](queries *T) *Repository[T] {
+func NewRepository[T any](queries T) *Repository[T] {
 	return &Repository[T]{Queries: queries}
 }
 
-func (r *Repository[T]) GetQueries() *T {
+func (r *Repository[T]) GetQueries() T {
 	return r.Queries
 }
 
-func (r *Repository[T]) WithQueries(queries *T) *Repository[T] {
+func (r *Repository[T]) WithQueries(queries T) *Repository[T] {
 	return &Repository[T]{Queries: queries}
 }
