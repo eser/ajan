a5fa1de460f5e51169cc5726e20940151e24d151
Eser Ozvataf <eser@acikyazilim.com>
2025-06-13 09:22:47 +0300

    feat: added missing config entries to baseconfig.

--- config.go
+++ config.go
@@ -3,20 +3,24 @@ package ajan
 import (
 	"github.com/eser/ajan/connfx"
 	"github.com/eser/ajan/grpcfx"
+	"github.com/eser/ajan/httpclient"
 	"github.com/eser/ajan/httpfx"
 	"github.com/eser/ajan/logfx"
+	"github.com/eser/ajan/metricsfx"
 )
 
 type BaseConfig struct {
-	AppName string `conf:"name" default:"ajansvc"`
-	AppEnv  string `conf:"env"  default:"development"`
+	Conn    connfx.Config `conf:"conn"`
+	AppName string        `conf:"name" default:"ajansvc"`
+	AppEnv  string        `conf:"env"  default:"development"`
 
 	// JwtSignature      string `conf:"jwt_signature"`
 	// CorsOrigin        string `conf:"cors_origin"`
 	// CorsStrictHeaders bool   `conf:"cors_strict_headers"`
 
-	Conn connfx.Config `conf:"conn"`
-	Log  logfx.Config  `conf:"log"`
-	GRPC grpcfx.Config `conf:"grpc"`
-	HTTP httpfx.Config `conf:"http"`
+	Log        logfx.Config      `conf:"log"`
+	Metrics    metricsfx.Config  `conf:"metrics"`
+	GRPC       grpcfx.Config     `conf:"grpc"`
+	HTTP       httpfx.Config     `conf:"http"`
+	HTTPClient httpclient.Config `conf:"http_client"`
 }
